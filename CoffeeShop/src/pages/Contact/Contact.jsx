b59b1077410ad823ea7597d4c45c07453b6a5c29
marc00000s <marcosdarwin.tulalian@tup.edu.ph>
2024-01-11 23:53:06 +0800

    Contact

--- CoffeeShop/src/pages/Contact/Contact.jsx
+++ CoffeeShop/src/pages/Contact/Contact.jsx
@@ -2,7 +2,23 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className="max-w-md w-full mx-auto p-6 bg-gray-800 rounded-lg shadow-md">
+      <h2 className="text-3x1 text-center text-pink-600 font-bold mb-6">Contact Us</h2>
+      <form action="">
+        <div className="mb-4">
+          <label className="FullName" htmlFor="">Your Name</label>
+          <input placeholder="John Wick" className="Name" required type="text"/>
+        </div>
+        <div className="mb-4">
+        <label className="Email" htmlFor="">Your Email</label>
+          <input placeholder="john@gmail.com" className="E-mail" required type="text"/>
+        </div>
+        <div className="mb-4">
+        <label className="Message" htmlFor=""> Your Message</label>
+          <input placeholder="Type your message here..." className="Mess" required type="text"/>
+        </div>
+      </form>
+    </div>
   )
 }
 
