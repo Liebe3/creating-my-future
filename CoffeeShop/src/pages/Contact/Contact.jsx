0bf2c74f48317451cb3b46f5c6a7f6ead12db244
marc00000s <marcosdarwin.tulalian@tup.edu.ph>
2024-01-12 00:02:30 +0800

    Update Contact.jsx

--- CoffeeShop/src/pages/Contact/Contact.jsx
+++ CoffeeShop/src/pages/Contact/Contact.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div className="max-w-md w-full mx-auto p-6 bg-gray-800 rounded-lg shadow-md">
-      <h2 className="text-3x1 text-center text-pink-600 font-bold mb-6">Contact Us</h2>
+    <div className="Contacts">
+      <h2>Contact Us</h2>
       <form action="">
         <div className="mb-4">
           <label className="FullName" htmlFor="">Your Name</label>
@@ -15,9 +15,15 @@ const Contact = () => {
         </div>
         <div className="mb-4">
         <label className="Message" htmlFor=""> Your Message</label>
-          <input placeholder="Type your message here..." className="Mess" required type="text"/>
+          <textarea rows='4' placeholder="Type your message here..." className="Mess" required type="text"/>
+        </div>
+        <div className="button-contact">
+          <button type="submit" className="enter">
+            Send Message
+          </button>
         </div>
       </form>
+
     </div>
   )
 }
