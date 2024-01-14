ab8f82b72abca89c3b6f900edb33975103cb9e64
raffyroad <raffyroad@gmail.com>
2024-01-14 16:18:01 +0800

    Update Coffee footer additional

---
+++ CoffeeShop/src/components/footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import style from "./footer.css
+
+const Footer = () => {
+  return (
+    <div className=" bg-gradient-to-r from-[#FFDCAB] to-[#AB6B2E] text-black rounded-t-3xl mt-8 md:mt-0">
+      <div className="flex flex-col md:flex-row justify-between p-8 md:px-32 px-5">
+        <div className=" w-full md:w-1/4">
+          <h1 className=" font-semibold text-xl pb-4">CafePulse</h1>
+          <p className=" text-sm">
+            Introducing -Shop Name- Enjoy quality time with us, your favorite people and love ones and coffee
+            here you can relax, and chill. Let out some steam and spark or spark some creativity
+          </p>
+        </div>
