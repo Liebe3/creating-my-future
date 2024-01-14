c6a1a40b9f29bb72d9a302d04c7840567d105524
marc00000s <marcosdarwin.tulalian@tup.edu.ph>
2024-01-15 02:53:42 +0800

    con

--- CoffeeShop/src/pages/Contact/Contact.jsx
+++ CoffeeShop/src/pages/Contact/Contact.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import contact1 from "../assets/contactImg/Calling_Monochromatic.svg";
 
 const Contact = () => {
-  return <div>Contacts</div>;
+  return (
+    <div className={style["contact-left"]}>
+      <div className={style["bottom-left"]}>
+          <img
+            className={style["contact-img1"]}
+            src={contact1}
+            alt="Contact-image1"
+          />
+      </div>
+    </div>
+
+
+);
 };
 
 export default Contact;
