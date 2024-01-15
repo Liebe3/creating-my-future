7b06aee74970b962356f45ff40d96190e5d4af59
raffyroad <raffyroad@gmail.com>
2024-01-15 16:26:08 +0800

    Menu Initialization

---
+++ CoffeeShop/src/components/Menu.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+// import img1 from "../assets/img/menu1.jpg";
+// import img2 from "../assets/img/menu2.jpg";
+// import img3 from "../assets/img/menu3.jpg";
+// import img4 from "../assets/img/menu4.jpg";
+// import img5 from "../assets/img/menu5.jpg";
+// import img6 from "../assets/img/menu6.jpg";
+// import MenuCard from "../layouts/MenuCard";
+
+const Menu = () => {
+    return (
+      <div className=" min-h-screen flex flex-col justify-center lg:px-32 px-5 bg-backgroundColor">
+        <h1 className=" font-semibold text-center text-4xl mt-24 mb-8">
+          Our Menu
+        </h1>
+  
