cd3927a8183c59cb69c484803b8ad948d36f1c66
raffyroad <raffyroad@gmail.com>
2024-01-21 16:36:59 +0800

    remove

--- CoffeeShop/src/components/Menu.jsx
+++ CoffeeShop/src/components/Menu.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 <link rel="stylesheet" href="coffeestyle.css"></link>
 
-import img1 from "../assets/coffee/coffee1.jpg";
+import img1 from "../assets/coffee/coffee1.jpeg";
 import img2 from "../assets/coffee/coffee2.jpg";
 import img3 from "../assets/coffee/coffee3.jpg";
 import img4 from "../assets/coffee/coffee4.jpg";
@@ -10,12 +10,12 @@ import MenuList from "../components/MenuList";
 
 const Menu = () => {
   return (
-    <div className=" min-h-screen flex flex-col justify-center lg:px-32 px-5 bg-backgroundColor">
-      <h1 className=" font-semibold text-center text-4xl mt-24 mb-8">
+    <div className>
+      <h1 className>
         Finest Selections
       </h1>
 
-      <div className=" flex flex-wrap pb-8 gap-8 justify-center">
+      <div className>
         
         <MenuList img={img1} title="Americano" />
         
