f2d1e8d2ac5150f0b7e0ad422b52c3545c5955f6
komiry <ryagondev@gmail.com>
2024-01-21 18:38:04 +0800

    ntabout
    notyet done

--- CoffeeShop/src/pages/About/About.jsx
+++ CoffeeShop/src/pages/About/About.jsx
@@ -1,15 +1,48 @@
-import React from "react";
-import style from "./About.module.css";
+import React, { useEffect } from 'react';
+import style from "./about.module.css";
 
 const About = () => {
   return (
-    <div>
+    <div className={style.section}>
       <section>
-        <div className="clock-container">
-          <h1>OPENING SOON! </h1>
-          <div id="clock"></div>
+        <div className={`${style.clockContainer} clock-container`}>
+          <h1>OPENING SOON!</h1>
+          <div id="clock" className={style.clock}>
+          </div>
         </div>
       </section>
+
+      {useEffect(() => {
+        const scrollAnimation = (entries, className) => {
+          entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+              entry.target.classList.add(className);
+            }
+          });
+        };
+
+        const Myclock = new IntersectionObserver((entries) => {
+          scrollAnimation(entries, style.clockShow);
+        });
+
+        const clock = document.querySelectorAll(`.${style.clockContainer}`);
+        clock.forEach((element) => {
+          Myclock.observe(element);
+        });
+
+        const countdown = setInterval(() => {
+          const currentTime = new Date();
+          let remainingHour = 24 - currentTime.getHours() - 1;
+          let remainingMinutes = 60 - currentTime.getMinutes();
+          let remainingSeconds = 60 - currentTime.getSeconds();
+          document.getElementById("clock").innerHTML = `${remainingHour} hours : ${remainingMinutes} minutes : ${remainingSeconds} seconds`;
+        }, 1000);
+
+        return () => {
+          clearInterval(countdown);
+          Myclock.disconnect();
+        };
+      }, [])}
     </div>
   );
 };
