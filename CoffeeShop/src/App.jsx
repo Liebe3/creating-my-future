0a8904be06d39e16d563d4f461be4a313d556075
Jallen Portugal <jallenqweqwe123@gmail.com>
2024-01-22 13:35:33 +0800

    added redirect file under public folder

--- CoffeeShop/src/App.jsx
+++ CoffeeShop/src/App.jsx
@@ -1,20 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/homeComponents/Navbar";
-import Products from "./pages/Products/Product";
 import About from "./pages/About/About";
 import Contact from "./pages/Contact/Contact";
 import BackgroundIMG from "./components/homeComponents/BackgroundIMG";
-import Maincomponents from "./components/homeComponents/Maincomponents";
 import Home from "./pages/Home/Home";
 import Cart from "./pages/Cart/Cart";
 import ShopContextProvider from "./context/ShopContextProvider";
 import Shop from "./pages/Products/Shop";
 import Ordernumber from "./pages/OrderNumber/Ordernumber";
-// import Cart from "./pages/Cart/Cart"
 function App() {
   return (
     <>
-      {/* <Navbar/> */}
       <div>
         <ShopContextProvider>
           <Router>
